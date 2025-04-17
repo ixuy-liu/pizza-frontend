b546ee1e3fe1efc2f8d12ca1807e51bbce82b52b
PranavThor <156145145+PranavThorali@users.noreply.github.com>
2025-04-17 19:36:04 -0400

    Update Home.js

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -5,7 +5,7 @@ function Home() {
     <div className="text-center">
       <h1 className="display-4">Welcome to PizzaTime</h1>
       <p className="lead">Fresh, delicious, and made to order. Explore our menu and satisfy your cravings!</p>
-      <img src="https://images.unsplash.com/photo-1601924582975-3ccde7c3d1b6" className="img-fluid rounded" alt="pizza" />
+      <img src="/images/pizza1.jpg" className="img-fluid rounded" alt="pizza" />
     </div>
   );
 }
